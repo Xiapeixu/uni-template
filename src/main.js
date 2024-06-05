1d754d3b55b154863eed0c881678b6c2fb19060b
夏培旭 <18530815565@163.com>
2024-06-05 17:12:32 +0800

    更新http

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import {
-	createSSRApp
-} from "vue";
+import { createSSRApp } from "vue";
+import http from "@/http/request.js";
 import App from "./App.vue";
 export function createApp() {
-	const app = createSSRApp(App);
-	return {
-		app,
-	};
+  const app = createSSRApp(App);
+  app.config.globalProperties.$http = http;
+  return {
+    app,
+  };
 }
